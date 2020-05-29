ee51dc557141dfa559fa371620253e193d937499
Graham Toppin <graham.toppin@gmail.com>
2020-05-29 19:07:54 -0400

    initial directory structure

---
+++ model/attribution_causal/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Models to explore our theories around attribution and causal relationships in multi-sided marketplaces',
+    author='Kartik and Graham',
+    license='BSD-3',
+)
