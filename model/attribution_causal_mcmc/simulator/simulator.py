ee4c03c72e95287a3a31a7ad7f5c950d86bcd611
Kartik Vamaraju <kartik.vamaraju@gmail.com>
2020-07-16 14:18:02 -0600

    Refactored simulator into class-based design.

--- model/attribution_causal_mcmc/simulator/simulator.py
+++ model/attribution_causal_mcmc/simulator/simulator.py
@@ -1,110 +1,166 @@
+from typing import List
+from enum import Enum
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Basic Simulation of Game between Content Consumer (User) and Platform
 
-def platform_select_items(inventory: list, subset_size: int, blacklist: set) -> np.array:
-    presented_items = np.random.choice(inventory, subset_size, replace=False)
 
-    while any([item in blacklist for item in presented_items]):
-        presented_items = np.random.choice(inventory, subset_size, replace=False)
-    
-    return presented_items
+class UserSelectionStrategy(Enum):
+    FIXED = 1,
+    UNIFORM = 2,
+    STRICT = 3
 
-def user_select_items(items: np.array, preferences: list, strategy: str, blacklist: set) -> int:
-    strategies = {'fixed', 'uniform', 'strict'}
 
-    if strategy not in strategies:
-        raise Exception('Unknown strategy in user_select_actions')
+class PlatformSelectionStrategy(Enum):
+    UNIFORM = 1,
 
-    if all([item in blacklist for item in items]):
-        raise Exception('No available item to select')
-    
-    if strategy == 'fixed':
+
+class InitialUserPreferenceStrategy(Enum):
+    IDENTICAL = 1,
+    UNIFORM = 2,
+
+
+class User:
+    def __init__(self, inventory_size: int, strategy: UserSelectionStrategy, update_blacklist: bool, initial_preferences: np.array, initial_blacklist: set):
+        self.preferences = initial_preferences or np.array(range(inventory_size))
+        self.update_blacklist = update_blacklist
+        self.blacklist = initial_blacklist
+        self._selection_history = []
+        self._selection_frequency = np.zeros(inventory_size)
+
+        if strategy == UserSelectionStrategy.FIXED:
+            self.selection_strategy = self._fixed_strategy
+        elif strategy == UserSelectionStrategy.UNIFORM:
+            self.selection_strategy = self._uniform_strategy
+        elif strategy == UserSelectionStrategy.STRICT:
+            self.selection_strategy = self._strict_strategy
+
+    def select_item(self, items: np.array) -> int:
+        if all([item in self.blacklist for item in items]):
+            return -1
+
+        selection = self.selection_strategy(items)
+
+        self._selection_frequency[selection] += 1
+        self._selection_history.append(selection)
+
+        if self.update_blacklist:
+            self.blacklist.add(selection)
+
+        return selection
+
+    @property
+    def selection_history(self):
+        return self._selection_history
+
+    @property
+    def selection_frequency(self):
+        return self._selection_frequency
+
+    def _fixed_strategy(self, items: np.array) -> int:
         selected_position = 0
 
-        while items[selected_position] in blacklist:
+        while items[selected_position] in self.blacklist:
             selected_position += 1
 
-    elif strategy == 'uniform':
-        n = len(items)
-        
-        selected_position = np.random.randint(n)
-        
-        while items[selected_position] in blacklist:
-            selected_position = np.random.randint(n)
-    
-    elif strategy == 'strict':
-        items = sorted(items, key=lambda x: preferences[x])
+        return items[selected_position]
+
+    def _uniform_strategy(self, items: np.array) -> int:
+        k = len(items)
+
+        selected_position = np.random.randint(k)
+
+        while items[selected_position] in self.blacklist:
+            selected_position = np.random.randint(k)
+
+        return items[selected_position]
+
+    def _strict_strategy(self, items: np.array) -> int:
         selected_position = 0
 
-        while items[selected_position] in blacklist:
+        items = sorted(items, key=lambda x: self.preferences[x])
+
+        while items[selected_position] in self.blacklist:
             selected_position += 1
 
-    return items[selected_position]
+        return items[selected_position]
 
-def simulate(inventory_size: int, inventory_subset_size: int, num_user_interactions: int, num_users: int, user_strategy: str, user_has_memory: bool, initial_user_preference_strategy: str, platform_strategy: str, platform_has_memory: bool, plot_results: bool) -> np.array:
-    inventory = list(range(inventory_size))
-    selection_probabiliity = np.zeros((num_users, inventory_size))
 
-    user_strategies = {'fixed', 'uniform', 'strict'}
-    platform_strategies = {'uniform'}
-    initial_user_preference_strategies = {'identical', 'uniform', 'clustered'}
+class Platform:
+    def __init__(self, inventory_size: int, presentation_size: int, strategy: PlatformSelectionStrategy, update_blacklist: bool):
+        self.inventory = list(range(inventory_size))
+        self.blacklist = [set()]
 
-    if user_strategy not in user_strategies or platform_strategy not in platform_strategies or initial_user_preference_strategy not in initial_user_preference_strategies:
-        raise Exception('Unsupported simulation strategy')
+        self.presentation_size = presentation_size
+        self.update_blacklist = update_blacklist
 
-    user_preferences = list(range(inventory_size))
+        if strategy == PlatformSelectionStrategy.UNIFORM:
+            self.selection_strategy = self._uniform_strategy
 
-    if user_has_memory or platform_has_memory:
-        for user in range(num_users):
-            user_item_blacklist = set()
-            platform_item_blacklist = set()
+    def add_user(self):
+        self.blacklist.append(set())
 
-            if initial_user_preference_strategy == 'uniform':
-                np.random.shuffle(user_preferences)
-            elif initial_user_preference_strategy == 'clustered':
-                choices = np.random.choice(inventory_size, 2, False)
-                user_preferences[choices[0]], user_preferences[choices[1]] = user_preferences[choices[1]], user_preferences[choices[0]]
+    def add_item_to_user_blacklist(self, item: int, user_id: int):
+        if self.update_blacklist:
+            self.blacklist[user_id].add(item)
 
+    def select_items_for_user(self, user_id: int) -> np.array:
+        return self.selection_strategy(user_id)
+
+    def _uniform_strategy(self, user_id: int) -> np.array:
+        presented_items = np.random.choice(self.inventory, self.presentation_size, replace=False)
+
+        while all([item in self.blacklist[user_id] for item in presented_items]):
+            presented_items = np.random.choice(self.inventory, self.presentation_size, replace=False)
+        return presented_items
+
+
+class Simulator:
+    def __init__(self,
+                 inventory_size: int,
+                 presentation_size: int,
+                 num_users: int,
+                 user_strategy:  UserSelectionStrategy,
+                 user_has_blacklist: bool,
+                 platform_strategy: PlatformSelectionStrategy,
+                 platform_has_blacklist: bool,
+                 initial_user_preference_strategy: InitialUserPreferenceStrategy):
+        self.inventory_size = inventory_size
+        self.num_users = num_users
+
+        if initial_user_preference_strategy == InitialUserPreferenceStrategy.IDENTICAL:
+            self.initial_user_preference_strategy = self._identical_initial_user_preference_strategy
+        elif initial_user_preference_strategy == InitialUserPreferenceStrategy.UNIFORM:
+            self.initial_user_preference_strategy = self._uniform_user_preference_strategy
+
+        self.users = [User(inventory_size, user_strategy, user_has_blacklist, self.initial_user_preference_strategy()[user], set()) for user in range(self.num_users)]
+
+        self.platform = Platform(inventory_size, presentation_size, platform_strategy, platform_has_blacklist)
+        for _ in range(self.num_users):
+            self.platform.add_user()
+
+    def simulate(self, num_user_interactions: int) -> np.array:
+        total_selection_frequency = np.zeros(self.inventory_size)
+        for user in range(self.num_users):
             for _ in range(num_user_interactions):
-                presented_items = platform_select_items(inventory, inventory_subset_size, platform_item_blacklist)
-                selected_item = user_select_items(presented_items, user_preferences, user_strategy, user_item_blacklist)
+                presented_items = self.platform.select_items_for_user(user)
+                selected_item = self.users[user].select_item(presented_items)
 
-                if user_has_memory:
-                    user_item_blacklist.add(selected_item)
-                
-                if platform_has_memory:
-                    platform_item_blacklist.add(selected_item)
+                self.platform.add_item_to_user_blacklist(selected_item, user)
 
-                selection_probabiliity[user][selected_item] += 1/num_user_interactions
-            
-            if initial_user_preference_strategy == 'clustered':
-                user_preferences[choices[0]], user_preferences[choices[1]] = user_preferences[choices[1]], user_preferences[choices[0]]
+            total_selection_frequency += self.users[user].selection_frequency
+        return total_selection_frequency
 
-    else:
-        for user in range(num_users):
+        np.random.shuffle(user_preferences)
 
-            if initial_user_preference_strategy == 'uniform':
-                np.random.shuffle(user_preferences)
-            elif initial_user_preference_strategy == 'clustered':
-                choices = np.random.choice(inventory_size, 2, False)
-                user_preferences[choices[0]], user_preferences[choices[1]] = user_preferences[choices[1]], user_preferences[choices[0]]
+        return [user_preferences.copy() for _ in range(self.num_users)]
 
-            for _ in range(num_user_interactions):
-                presented_items = platform_select_items(inventory, inventory_subset_size, set())
-                selected_item = user_select_items(presented_items, user_preferences, user_strategy, set())
-
-                selection_probabiliity[user][selected_item] += 1/num_user_interactions
-
-            if initial_user_preference_strategy == 'clustered':
-                user_preferences[choices[0]], user_preferences[choices[1]] = user_preferences[choices[1]], user_preferences[choices[0]]
-
-    if plot_results:
-        plt.bar(list(range(inventory_size)), np.mean(selection_probabiliity, axis=0))
-        plt.show()
-    
-    return selection_probabiliity
+    def _uniform_user_preference_strategy(self) -> List[np.array]:
+        user_preferences = [list(range(self.inventory_size)) for _ in range(self.num_users)]
+        for user in range(self.num_users):
+            np.random.shuffle(user_preferences[user])
+        return user_preferences
 
 # """
 # Assumptions:
@@ -113,7 +169,7 @@ def simulate(inventory_size: int, inventory_subset_size: int, num_user_interacti
 # - All Users follow identical selection strategies to one another
 # """
 
-# simulate(10000, 8, 100, 100, 'fixed', False, 'identical', 'uniform', False, True)
+# simulator = Simulator(10000, 8, 100, UserSelectionStrategy.FIXED, False, PlatformSelectionStrategy.UNIFORM, False, InitialUserPreferenceStrategy.IDENTICAL)
 
 # """
 # Assumptions:
@@ -122,7 +178,9 @@ def simulate(inventory_size: int, inventory_subset_size: int, num_user_interacti
 # - All Users follow identical selection strategies to one another
 # """
 
-# simulate(10000, 8, 100, 100, 'uniform', False, 'identical', 'uniform', False, True)
+# simulate(10000, 8, 10    def _identical_initial_user_preference_strategy(self) -> List[np.array]:
+        user_preferences = list(range(self.inventory_size))0, 100, 'uniform', False, 'identical', 'uniform', False, True)
+# simulator = Simulator(10000, 8, 100, UserSelectionStrategy.UNIFORM, False, PlatformSelectionStrategy.UNIFORM, False, InitialUserPreferenceStrategy.IDENTICAL)
 
 # """
 # Assumptions:
@@ -131,7 +189,7 @@ def simulate(inventory_size: int, inventory_subset_size: int, num_user_interacti
 # - All Users follow identical selection strategies to one another
 # """
 
-# simulate(10000, 8, 100, 100, 'uniform', True, 'identical', 'uniform', False, True)
+# simulator = Simulator(10000, 8, 100, UserSelectionStrategy.UNIFORM, True, PlatformSelectionStrategy.UNIFORM, False, InitialUserPreferenceStrategy.IDENTICAL)
 
 # """
 # Assumptions:
@@ -140,16 +198,7 @@ def simulate(inventory_size: int, inventory_subset_size: int, num_user_interacti
 # - All Users follow identical selection strategies to one another
 # """
 
-# simulate(10000, 8, 100, 100, 'uniform', False, 'identical', 'uniform', True, True)
-
-# """
-# Assumptions:
-# - Platform selects subsets of items uniformly, but only from the subset of items that the User hasn't selected before
-# - User always selects actions uniformly, but will not select an action that they have already selected in previous interactions
-# - All Users follow identical selection strategies to one another
-# """
-
-# simulate(10000, 8, 100, 100, 'uniform', True, 'identical', 'uniform', True, True)
+# simulator = Simulator(10000, 8, 100, UserSelectionStrategy.UNIFORM, True, PlatformSelectionStrategy.UNIFORM, True, InitialUserPreferenceStrategy.IDENTICAL)
 
 # """
 # Assumptions:
@@ -158,7 +207,7 @@ def simulate(inventory_size: int, inventory_subset_size: int, num_user_interacti
 # - All Users follow identical selection strategies to one another
 # """
 
-# simulate(10000, 8, 100, 100, 'strict', False, 'identical', 'uniform', False, True)
+# simulator = Simulator(10000, 8, 100, UserSelectionStrategy.STRICT, False, PlatformSelectionStrategy.UNIFORM, False, InitialUserPreferenceStrategy.IDENTICAL)
 
 # """
 # Assumptions:
@@ -167,7 +216,7 @@ def simulate(inventory_size: int, inventory_subset_size: int, num_user_interacti
 # - All Users follow identical selection strategies to one another
 # """
 
-# simulate(10000, 8, 100, 100, 'strict', True, 'identical', 'uniform', False, True)
+# simulator = Simulator(10000, 8, 100, UserSelectionStrategy.STRICT, True, PlatformSelectionStrategy.UNIFORM, False, InitialUserPreferenceStrategy.IDENTICAL)
 
 # """
 # Assumptions:
@@ -176,7 +225,7 @@ def simulate(inventory_size: int, inventory_subset_size: int, num_user_interacti
 # - All Users follow identical selection strategies to one another
 # """
 
-# simulate(10000, 8, 100, 100, 'strict', True, 'identical', 'uniform', True, True)
+# simulator = Simulator(10000, 8, 100, UserSelectionStrategy.STRICT, True, PlatformSelectionStrategy.UNIFORM, True, InitialUserPreferenceStrategy.IDENTICAL)
 
 # """
 # Assumptions:
@@ -185,13 +234,11 @@ def simulate(inventory_size: int, inventory_subset_size: int, num_user_interacti
 # - All Users follow identical selection strategies to one another, but with different preferences. The preferences are uniformly distributed.
 # """
 
-# simulate(10000, 8, 100, 100, 'strict', True, 'uniform', 'uniform', True, True)
+simulator = Simulator(10000, 8, 100, UserSelectionStrategy.STRICT, True, PlatformSelectionStrategy.UNIFORM, True, InitialUserPreferenceStrategy.UNIFORM)
 
-# """
-# Assumptions:
-# - Platform selects subsets of items uniformly, but only from the subset of items that the User hasn't selected before
-# - User always selects items based on strict preferences but does not select the same item twice
-# - All Users follow identical selection strategies to one another, but with different preferences. The preferences are clustered together.
-# """
 
-simulate(10000, 8, 100, 100, 'strict', True, 'clustered', 'uniform', True, True)
+total_selection_frequency = simulator.simulate(100)
+
+
+plt.bar(list(range(10000)), total_selection_frequency)
+plt.show()
